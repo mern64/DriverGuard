79c9c9d187b677801ac6156fa3c0c4a155b3982e
Imran Mansor <mern99865@gmail.com>
2026-01-11 22:26:24 +0800

    Initial commit: DriverGuard System (Clean)

---
+++ STINK3014_A251_Project/Pack01a_BaselineCode/STINK3014_CaseStudy_FacialExpressionDetection_Dev.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2b06e88f367c8f52a294e3be4a2ce8140f5a51c5159689a0ee91a9a79f8696a7
+size 3570
---
+++ STINK3014_A251_Project/Pack01b_DeploymentCode/STINK3014_CaseStudy_FacialExpressionDetection_App.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec7a0b8f3e8c1fd531d071fff955c6eb40536ac8a8231435fe1b6e484005714f
+size 11038
